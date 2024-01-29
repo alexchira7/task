5768eeb9bc98a1d2f62c51efc87ed7142f27d92a
qwe1 <cherevkov-l@mail.ru>
2024-01-29 16:54:28 +0400

    1:condition_cycles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,110 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        String name ="Алексей";
+//        byte age = 36;
+//        System.out.println("меня зовут " + name + ", мне " + age + " лет");
+
+//        short m = 32000;
+//        System.out.println("Monitor ctoit " + m + " rubley");
+
+
+
+//        String kolichestvoBanok = "x";
+//        String whitePaint = "a";
+//        String brownPaint = "b";
+//        byte x = 120;      // на ремонт школы требуется такое количество банок
+//        byte a = 2;       // на 1 класс требуется столько банок
+//        byte b = 4;       // на 1 класс требуется столько банок
+//        System.out.println("В школе, где на " + (x / (a + b)) + " классов, нужно " + (x / (a + b)) * a + " банок белой краски и " + (x / (a + b)) * b + " банок коричневой краски");
+
+
+//        String Masha = "q";
+//        String Denis = "w";
+//        String Kristina = "e";
+//        String GodovoyDohod = "r";
+//        int q = 67760;
+//        int w = 83690;
+//        int e = 76230;
+//        double r = 0.1;
+//        System.out.println("Годовой доход Маши вырос на " + q * r + " рублей и теперь она получает " + ((q * r) + q) + " рублей");
+//        System.out.println("Годовой доход Дениса вырос на " + w * r + " рублей и теперь он получает " + ((w * r) + w) + " рублей");
+//        System.out.println("Годовой доход Кристины вырос на " + e * r + " рублей и теперь она получает " +((e * r) + e) + " рублей");
+
+
+
+//        String gradus = "g";
+//        String holodno = "h";
+//        String teplo = "t";
+//        int g = 5;
+//        int h = -5;
+//        int t = 10;
+//        if (h > g) {
+//            System.out.println("На улице " + h + " градусов нужно надеть шапку");
+//        } else if (h < g) {
+//            System.out.println("На улице " + t + " градусов можно идти без шапки");
+//        }
+//        if (h < g) {
+//            System.out.println("На улице " + h + " градусов нужно надеть шапку");
+//        } else if (h > g) {
+//            System.out.println("На улице " + t + " градусов можно идти без шапки");
+//        }
+
+//        int monthNumber = 5;
+//        switch (monthNumber) {
+//            case 12:
+//            case 1:
+//            case 2:
+//                System.out.println("Декабрь, Январь или Февраль");
+//                System.out.println("принадлежит к сезону зима");
+//                break;
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Март, Апрель или Май");
+//                System.out.println("принадлежит к сезону весна");
+//                break;
+//            case 6:
+//            case 7:
+//            case 8:
+//                System.out.println("Июнь, Июль или Август");
+//                System.out.println("принадлежит к сезону лето");
+//                break;
+//            case 9:
+//            case 10:
+//            case 11:
+//                System.out.println("Сентябрь, Октябрь или Ноябрь");
+//                System.out.println("принадлежит к сезону осень");
+//                break;
+//            default:
+//                System.out.println("Такого месяца нет");
+//        }
+
+
+//        int i = 2;
+//        while(i <= 17) {
+//            if (i % 2 == 0)
+//            i += 2;
+//            System.out.println(i);
+
+
+//        for(int i = 10; i >= 1; i--) {
+//            System.out.println(i);
+//        }
+
+
+//        int j = 0;
+//        int h = 15000;
+//        while (j <= 2459000) {
+//            j += h ;
+//        System.out.println(j);
+
+//        }
+
+
     }
+
 }
+
+
+
+
