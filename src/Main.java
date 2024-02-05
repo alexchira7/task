c63fdfeb0b2f8c4adaa3f8fcd4cc455822905679
qwe1 <cherevkov-l@mail.ru>
2024-02-05 20:24:33 +0400

    feature/2-condition_cycles

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+//        domashka();
+//        task1();
+//        task2();
+//        task3();
+          task4();
+
+    }
+
+//    static void domashka() {
+//
 //        String name ="Алексей";
 //        byte age = 36;
 //        System.out.println("меня зовут " + name + ", мне " + age + " лет");
-
+//
 //        short m = 32000;
 //        System.out.println("Monitor ctoit " + m + " rubley");
-
-
-
+//
+//
 //        String kolichestvoBanok = "x";
 //        String whitePaint = "a";
 //        String brownPaint = "b";
@@ -16,8 +28,8 @@ public class Main {
 //        byte a = 2;       // на 1 класс требуется столько банок
 //        byte b = 4;       // на 1 класс требуется столько банок
 //        System.out.println("В школе, где на " + (x / (a + b)) + " классов, нужно " + (x / (a + b)) * a + " банок белой краски и " + (x / (a + b)) * b + " банок коричневой краски");
-
-
+//
+//
 //        String Masha = "q";
 //        String Denis = "w";
 //        String Kristina = "e";
@@ -29,82 +41,82 @@ public class Main {
 //        System.out.println("Годовой доход Маши вырос на " + q * r + " рублей и теперь она получает " + ((q * r) + q) + " рублей");
 //        System.out.println("Годовой доход Дениса вырос на " + w * r + " рублей и теперь он получает " + ((w * r) + w) + " рублей");
 //        System.out.println("Годовой доход Кристины вырос на " + e * r + " рублей и теперь она получает " +((e * r) + e) + " рублей");
-
-
-
-//        String gradus = "g";
+//
+//
+//       String gradus = "g";
 //        String holodno = "h";
-//        String teplo = "t";
+//      String teplo = "t";
 //        int g = 5;
-//        int h = -5;
+//      int h = -5;
 //        int t = 10;
-//        if (h > g) {
-//            System.out.println("На улице " + h + " градусов нужно надеть шапку");
-//        } else if (h < g) {
-//            System.out.println("На улице " + t + " градусов можно идти без шапки");
+////        System.out.println("Месяц " + m + " , сумма накоплений равно " + j + " рублей");
+//    }
+
+//     static void task1() {
+//
+//        int[] arr = new int[8];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = (int)(Math.random() * 8)+1;
 //        }
-//        if (h < g) {
-//            System.out.println("На улице " + h + " градусов нужно надеть шапку");
-//        } else if (h > g) {
-//            System.out.println("На улице " + t + " градусов можно идти без шапки");
+//        System.out.println(Arrays.toString(arr));
+
+//    }
+
+//     static void task2() {
+//        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i = 0; i < arr1.length; i++) {
+//            if (arr1[i] % 2 != 0) {
+//                System.out.println(arr1[i]);
+//            }
+//
 //        }
+//
+//    }
 
-//        int monthNumber = 5;
-//        switch (monthNumber) {
-//            case 12:
-//            case 1:
-//            case 2:
-//                System.out.println("Декабрь, Январь или Февраль");
-//                System.out.println("принадлежит к сезону зима");
-//                break;
-//            case 3:
-//            case 4:
-//            case 5:
-//                System.out.println("Март, Апрель или Май");
-//                System.out.println("принадлежит к сезону весна");
-//                break;
-//            case 6:
-//            case 7:
-//            case 8:
-//                System.out.println("Июнь, Июль или Август");
-//                System.out.println("принадлежит к сезону лето");
-//                break;
-//            case 9:
-//            case 10:
-//            case 11:
-//                System.out.println("Сентябрь, Октябрь или Ноябрь");
-//                System.out.println("принадлежит к сезону осень");
-//                break;
-//            default:
-//                System.out.println("Такого месяца нет");
+//     static void task3() {
+//        double[] arr2 = {3.,5.,7.,12.};
+//        double sum = 0.;
+//        int index = 0;
+//        while(index <arr2.length) {
+//            sum += arr2[index];
+//            index++;
 //        }
+//        System.out.println(sum);
+//    }
 
-
-//        int i = 2;
-//        while(i <= 17) {
-//            if (i % 2 == 0)
-//            i += 2;
-//            System.out.println(i);
-
-
-//        for(int i = 10; i >= 1; i--) {
-//            System.out.println(i);
+//     static double task3() {
+//
+//        double arr3[] = { 3.4, 5.9, 7.0, 12.6 };
+//        int i = 0;
+//        double sum = 0;
+//        while (i < arr3.length) {
+//            sum += arr3[i];
+//           i++;
 //        }
+//         System.out.println(sum);
+//         return sum;
+//     }
+//
+//
 
-
-        int j = 0;
-        int m = 0;
-        int h = 15000;
-        while (j < 2459000) {
-            j += h; m++; }
-        System.out.println("Месяц " + m + " , сумма накоплений равно " + j + " рублей");
-
-
-
-
+    public static void task4() {
+        String[] arr4 = new String[]{"Я", "учусь", "писать", "Hello", "World", "!!!"};
+        String myString = concat(arr4);
+        System.out.println(myString);
     }
 
+    static String concat(String[] myArr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < myArr.length; i++) {
+            sb.append(myArr[i]);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
+
+
 
 
 
